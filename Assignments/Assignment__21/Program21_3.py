49a81915d974d9991c0fe1187123a6ee5d0266e9
MrunmaiK5 <mrunmai0205@gmail.com>
2026-01-27 13:09:12 +0530

    Added Assignment__21

--- Assignments/Assignment__21/Program21_3.py
+++ Assignments/Assignment__21/Program21_3.py
@@ -4,7 +4,7 @@ SharedCounter = 0
 lock = threading.Lock()
 
 #######################################################################################
-#   Function name   :   Thread1, Thread2
+#   Function name   :   Thread1, Thread2, Thread3, Thread4
 #   Description     :   Updates the shared counter.
 #   Input           :   Nothing
 #   Output          :   Nothing
@@ -15,31 +15,52 @@ lock = threading.Lock()
 def Thread1():
     global SharedCounter
 
-    for i in range(500):
+    for i in range(10000):
         lock.acquire()
-        SharedCounter += 3
+        SharedCounter += 2
         lock.release()
 
 def Thread2():
     global SharedCounter
 
-    for i in range(500):
+    for i in range(10000):
         lock.acquire()
         SharedCounter -=1
+        lock.release()
+
+def Thread3():
+    global SharedCounter
+
+    for i in range(10000):
+        lock.acquire()
+        SharedCounter += 2
+        lock.release()
+
+def Thread4():
+    global SharedCounter
+
+    for i in range(10000):
+        lock.acquire()
+        SharedCounter += 1
         lock.release()
 
 def main(): 
     t1 = threading.Thread(target=Thread1)
     t2 = threading.Thread(target=Thread2)
+    t3 = threading.Thread(target=Thread3)
+    t4 = threading.Thread(target=Thread3)
     
     t1.start()
     t2.start()
+    t3.start()
+    t4.start()
 
     t1.join()
     t2.join()
+    t3.join()
+    t4.join()
 
     print("Counter :",SharedCounter)
-
 
 if __name__ == "__main__":
     main()
